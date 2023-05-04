554f9e70305ce487f8cf6d85924eba4a3824e3a4
hc <tt12b89@gmail.com>
2023-05-04 19:01:22 +0900

    commit Test

--- src/main/java/ywluv/bcmProject/controller/TesController2.java
+++ src/main/java/ywluv/bcmProject/controller/TesController2.java
@@ -7,6 +7,7 @@ public class TesController2 {
 
     public String test(String test){
         System.out.println("test = " + test);
+        System.out.println("\"12345\" = " + "12345");
         return null;
     }
 }

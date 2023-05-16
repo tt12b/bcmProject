2a5e1a987096973925441455334923e3a938e297
ywluv <tt12b89@gmail.com>
2023-05-16 17:01:45 +0900

    0516

--- src/main/java/ywluv/bcmProject/controller/MainController.java
+++ src/main/java/ywluv/bcmProject/controller/MainController.java
@@ -15,7 +15,6 @@ import java.util.List;
 @Controller
 public class MainController {
 
-    @Autowired MemberClubService memberClubService;
 
 
     @GetMapping("/")
@@ -24,13 +23,5 @@ public class MainController {
 
     }
 
-    @GetMapping("/memberList")
-    public String home(Model model){
-        List<MemberDto> memberInfo = memberClubService.findMemberClubs();
 
-        model.addAttribute("memberInfo",memberInfo);
-
-        return "member/memberList";
-
-    }
 }
--- src/main/java/ywluv/bcmProject/controller/MemberController.java
+++ src/main/java/ywluv/bcmProject/controller/MemberController.java
@@ -1,12 +1,28 @@
 package ywluv.bcmProject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ywluv.bcmProject.service.MemberClubService;
+import ywluv.dto.MemberDto;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
 
+    @Autowired MemberClubService memberClubService;
+
+    @GetMapping("/memberList")
+    public String home(Model model){
+        List<MemberDto> memberInfo = memberClubService.findMemberClubs();
+
+        model.addAttribute("memberInfo",memberInfo);
+
+        return "member/memberList";
+
+    }
     @GetMapping("/memberRegister")
     public String memberRegister(Model model){
 
@@ -14,4 +30,6 @@ public class MemberController {
         return "member/memberRegister";
 
     }
+
+
 }

9c4df3993c88ccc42a093d39803195792f86df71
hc <tt12b89@gmail.com>
2023-05-30 14:46:39 +0900

    0530

--- src/main/java/ywluv/bcmProject/controller/MeetupController.java
+++ src/main/java/ywluv/bcmProject/controller/MeetupController.java
@@ -2,14 +2,17 @@ package ywluv.bcmProject.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ywluv.bcmProject.util.DateUtil;
 
 @Controller
 @RequiredArgsConstructor
 public class MeetupController {
 
     @GetMapping("/meetup")
-    public String meetupList(){
+    public String meetupList(Model model){
+        model.addAttribute("localDate", DateUtil.getCurrentDate());
 
         return "meetup/meetupList";
 
